a5deba57bb48919db105fa6aecfc2c9119c63a00
baltasar benitez <baltaarex363@gmail.com>
2024-05-29 20:50:23 -0300

    ne

--- .netlify/functions-internal/ssr/chunks/generic_BtgIQJq9.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_DGhhIoa7.mjs');
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_CV62J4AK.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_Cek_L70v.mjs');
-
-export { page };
